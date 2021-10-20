b3462e48bc6e865be3d7bd4f79e7243a9a8ca995
TJ-developer <tjark@andermann.eu>
2021-10-20 16:47:32 +0200

    Init

---
+++ src/main/java/eu/andermann/festivalappdemo/FestivalAppDemoApplication.java
@@ -0,0 +1,13 @@
+package eu.andermann.festivalappdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FestivalAppDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FestivalAppDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/eu/andermann/festivalappdemo/FestivalAppDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.andermann.festivalappdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FestivalAppDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
